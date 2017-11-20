27b6e9e6c61a292c58a8364a83897d0e96b66102
Armand Grillet <armand.grillet@outlook.com>
2017-11-20 11:54:03 +0100

    Removed logging.

--- records/state/client/cli.go
+++ records/state/client/cli.go
@@ -44,9 +44,7 @@ func LoadMasterStateTryAll(masters []string, stateLoader func(ip, port string) (
 	if len(masters) > 0 {
 		leader, masters = masters[0], masters[1:]
 	} else {
-		err = errors.New("No masters given to fetch state.json")
-		logging.Error.Println(err)
-		return sj, err
+		return sj, errors.New("No masters given to fetch state.json")
 	}
 
 	// Check if ZK leader is correct
